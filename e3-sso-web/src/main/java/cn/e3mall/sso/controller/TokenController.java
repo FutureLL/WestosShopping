4310c0b8f20b1cb7a69221edbdce14c46e43b9fa
master <13186102535@163.com>
2018-12-23 23:12:39 +0800

    根据token取用户信息，并测试

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/TokenController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/TokenController.java
@@ -22,6 +22,14 @@ public class TokenController {
     @Autowired
     private TokenService tokenService;
 
+    /**
+     * 网页进行测试：localhost:1028/user/token/08fa2c76-6f41-4e32-8298-4dc19c6626b5
+     * 得到：{"status":200,"msg":"OK","data":{"id":9,"username":"lilei","password":null,"phone":"13186195292","email":null,"created":1545480349000,"updated":1545480349000}}
+     *
+     * 网页进行测试：localhost:1028/user/token/08fa2c76-6f41-4e32-8298-4dc19c6626b0
+     * 得到：{"status":201,"msg":"用户登录已经过期","data":null}
+     */
+
     @RequestMapping("/user/token/{token}")
     @ResponseBody
     public E3Result getUserByToken(@PathVariable String token){
